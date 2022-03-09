550848c0b9e765eae5e27f7bb6593953f3e5a5de
erewl <grunert.katrin@protonmail.com>
2022-03-09 16:56:23 +0100

    Initial Landing Page setup

---
+++ src/data/content-data.js
@@ -0,0 +1,4 @@
+export default {
+    "mainTitle": "Andrea",
+    "subTitle": "The one-stop solution for property management with smart learning capabilities"
+}
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          lighter: '#fecdd3',
+          default: '#f43f5e',
+          darker: '#be123c',
+
+          50: '#fff1f2',
+          100: '#ffe4e6',
+          200: '#fecdd3',
+          300: '#fda4af',
+          400: '#fb7185',
+          500: '#f43f5e',
+          600: '#e11d48',
+          700: '#be123c',
+          800: '#9f1239',
+          900: '#881337'
+        },
+        colors: {
+          transparent: 'transparent',
+          current: 'currentColor',
+          'white': '#ffffff'
+        }
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
