a782dc7809805730443910643f1667b8dc4c8734
erewl <grunert.katrin@protonmail.com>
2022-03-09 17:00:01 +0100

    Updates

---
+++ component---src-pages-404-js-b67a4774a26aa8245d97.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_lander=self.webpackChunkgatsby_starter_lander||[]).push([[883],{429:function(t,e,n){n.r(e);n(7294);var u=n(8665),a=n(1100);e.default=function(){return(0,a.tZ)(u.Z,null,(0,a.tZ)("h1",null,"Not Found"),(0,a.tZ)("p",null,"You just hit a page that doesn't exist..."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-b67a4774a26aa8245d97.js.map
--- src/data/content-data.js
+++
@@ -1,4 +0,0 @@
-export default {
-    "mainTitle": "Andrea",
-    "subTitle": "The one-stop solution for property management with smart learning capabilities"
-}
