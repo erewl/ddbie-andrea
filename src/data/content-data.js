78cdacc5cdf115bb1b95b5ce9911e7ed7b438cd1
erewl <grunert.katrin@protonmail.com>
2022-03-11 13:48:43 +0100

    Modify content, Add some real estate related graphics

--- src/data/content-data.js
+++ src/data/content-data.js
@@ -1,4 +1,18 @@
 export default {
     "mainTitle": "Andrea",
-    "subTitle": "The one-stop solution for property management with smart learning capabilities"
+    "subTitle": "The one-stop solution for property management with smart learning capabilities.",
+    "features": [
+        {
+            "title": "Inspection Support",
+            "description": "Never were inspections easier! With a customisable form template Andrea will guide you through the inspection, uploading all data to the platform of your choice."
+        },
+        {
+            "title": "Overview",
+            "description": "With a modern dashboard yada yada yada"
+        },
+        {
+            "title": "Predictive Maintance",
+            "description": "An AI powered system will predict when maintenance are due and notify the affected parties in no time."
+        }
+    ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -35,35 +35,18 @@ const Index = () => (
     </section>
     <section id="features" className="py-20 lg:pb-40 lg:pt-48">
       <div className="container mx-auto text-center">
-        <h2 className="text-3xl lg:text-5xl font-semibold">Main Features</h2>
+        <h2 className="text-3xl lg:text-5xl font-semibold">We offer</h2>
         <div className="flex flex-col sm:flex-row sm:-mx-3 mt-12">
-          <div className="flex-1 px-3">
-            <Card className="mb-8">
-              <p className="font-semibold text-xl">Service One</p>
-              <p className="mt-4">
-                An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
-                velna vitae auctor integer.
-              </p>
-            </Card>
-          </div>
-          <div className="flex-1 px-3">
-            <Card className="mb-8">
-              <p className="font-semibold text-xl">Service Two</p>
-              <p className="mt-4">
-                An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
-                velna vitae auctor integer.
-              </p>
-            </Card>
-          </div>
-          <div className="flex-1 px-3">
-            <Card className="mb-8">
-              <p className="font-semibold text-xl">Service Three</p>
-              <p className="mt-4">
-                An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
-                velna vitae auctor integer.
-              </p>
-            </Card>
-          </div>
+          {data.features.map(feature => {
+            return <div className="flex-1 px-3">
+              <Card className="mb-8">
+                <p className="font-semibold text-xl">{ feature.title}</p>
+                <p className="mt-4">
+                  { feature. description }
+                </p>
+              </Card>
+            </div>
+          })}
         </div>
       </div>
     </section>
